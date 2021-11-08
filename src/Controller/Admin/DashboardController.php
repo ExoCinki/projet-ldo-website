8503bc0ee231e1d353a99e99ab035b865bd6875a
ExoCinki <aolacot@gmail.com>
2021-11-08 15:41:56 +0100

    Base

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\News;
+use App\Entity\User;
+use App\Entity\Gemme;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
