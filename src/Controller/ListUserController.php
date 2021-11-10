8f5ea50b6ff0135e1fc1eb33517d0cdbf587257c
ExoCinki <aolacot@gmail.com>
2021-11-10 19:07:48 +0100

    Recensement

---
+++ src/Controller/ListUserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ListUserController extends AbstractController
+{
+    /**
+     * @Route("/list/user", name="list_user")
+     * @IsGranted("ROLE_USER")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+        $userlist = $userRepository->findAll();
+        return $this->render('list_user/index.html.twig', [
+            'userlist' => $userlist,
+        ]);
+    }
+}
